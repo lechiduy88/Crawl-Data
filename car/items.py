5cd1005eea8b6e7aa6a224e035226ba2fff72711
lechiduy <lechiduy2002@gmail.com>
2023-09-07 14:50:19 +0700

    first commit

---
+++ car/items.py
@@ -0,0 +1,22 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class CarItem(scrapy.Item):
+    # define the fields for your item here like:
+    name = scrapy.Field() # Tên xe
+    price = scrapy.Field() # Giá xe
+    origin = scrapy.Field() #Xuất xứ 
+    color = scrapy.Field() # Màu xe
+    KMtraveled = scrapy.Field() #Số km đã đi
+    status = scrapy.Field() #Tình trạng
+    doorNumber = scrapy.Field() #Số cửa 
+    seatNumber = scrapy.Field # Số chỗ ngồi 
+    engine = scrapy.Field() # Động cơ
+    gear = scrapy.Field() #Hộp số
+    fuelConsumption = scrapy.Field() #Mức độ tiêu thụ nguyên liệu
+    pass
---
+++ car/spiders/oldCars.py
@@ -0,0 +1,54 @@
+import scrapy
+from car.items import CarItem
+
+
+class OldcarsSpider(scrapy.Spider):
+    name = "oldCars"
+    allowed_domains = ["sanbonbanh.com"]
+    start_urls = ["https://sanbonbanh.com/"]
+
+    def start_requests(self):
+        # s = ""
+        # e = ""
+        pages = []
+        for i in range(17,19): 
+            domain = 'https://sanbonbanh.com/sellcar/page-{}/'.format(i)
+            pages.append(domain)
+
+        for page in pages:
+            yield scrapy.Request(url=page, callback=self.parse_link)
+    def parse_link(self, response):
+        for i in range(1, 21):
+            str = 'body > section.product-listing.page-section-ptb > div > div > div.col-lg-8.col-md-7.bg-white > div.list-group > div:nth-child({}) > div > div.col-lg-8.col-md-12 > div > div.car-title > a::attr(href)'.format(i)
+            #      body > section.product-listing.page-section-ptb > div > div > div.col-lg-8.col-md-7.bg-white > div.list-group > div:nth-child(2) > div > div.col-lg-8.col-md-12 > div > div.car-title > a
+            #      body > section.product-listing.page-section-ptb > div > div > div.col-lg-8.col-md-7.bg-white > div.list-group > div:nth-child(1) > div > div.col-lg-8.col-md-12 > div > div.car-title > a
+            link = response.css(str).extract_first()
+
+            yield scrapy.Request(response.urljoin(link), callback=self.parse)
+
+    def parse(self, response):
+        item = CarItem()
+        item['name'] = response.xpath(
+            '/html/body/section[2]/div/div[1]/div[1]/h3/text()').extract_first() 
+        item['price'] = response.xpath(
+            '/html/body/section[2]/div/div[1]/div[2]/div[1]/strong/text()').extract_first() 
+        item['origin'] = response.xpath(
+            '/html/body/section[2]/div/div[2]/div[2]/div/div/ul/li[5]/strong/text()').extract_first() 
+        item['color'] = response.xpath(
+            '/html/body/section[2]/div/div[3]/div[1]/div[1]/div[1]/table/tbody/tr[4]/td/text()').extract_first() 
+        item['KMtraveled'] = response.xpath(
+            '/html/body/section[2]/div/div[2]/div[2]/div/div/ul/li[8]/strong/text()').extract_first() 
+        item['status'] = response.xpath(
+            '/html/body/section[2]/div/div[2]/div[2]/div/div/ul/li[2]/strong/text()').extract_first() 
+        item['doorNumber'] = response.xpath(
+            '/html/body/section[2]/div/div[3]/div[1]/div[1]/div[1]/table/tbody/tr[2]/td/text()').extract_first() 
+        # item['seatNumber'] = response.xpath(
+        #     '/html/body/section[2]/div/div[3]/div[1]/div[1]/div[1]/table/tbody/tr[1]/td/text()').extract_first() 
+        item['engine'] = response.xpath(
+            '/html/body/section[2]/div/div[2]/div[2]/div/div/ul/li[9]/strong/text()').extract_first() 
+        item['gear'] = response.xpath(
+            '/html/body/section[2]/div/div[3]/div[1]/div[1]/div[1]/table/tbody/tr[5]/td/text()').extract_first() 
+        item['fuelConsumption'] = response.xpath(
+            '/html/body/section[2]/div/div[3]/div[1]/div[1]/div[1]/table/tbody/tr[8]/td/text()').extract_first() 
+        
+        yield item
