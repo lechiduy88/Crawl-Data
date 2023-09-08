5178ebbd86f7b88c92fcfca05f3a2c744f2b650d
lechiduy <lechiduy2002@gmail.com>
2023-09-08 10:12:56 +0700

    update5

--- cleaningData.py
+++ cleaningData.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     df = pd.read_csv('./car/car/car.csv', error_bad_lines=False)
     df = df[['name','price','origin','status','KMtraveled','color','doorNumber','engine','fuelConsumption']]
     df = df[df.name.notnull()]
-    df.to_csv('./car/allcar.csv', sep='\t', encoding='utf-8')
+    df.to_csv('./car/allcar.csv', sep=',', encoding='utf-8')
